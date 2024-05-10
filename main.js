92e1b35d0ce68477485d5de65c2b78833ec4147c
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-05-10 19:44:25 +0500

    add these files

---
+++ main.js
@@ -0,0 +1,10 @@
+// for (let a = 1; a <= 100; ++a) {
+//   console.log("Thank you Governor Sindh Kamran Khan Tessori");
+// }
+var cars = ["Rolls Royce", "Bently", "Ferrari", "BMW"];
+console.log("My favourite car is " + cars[1]);
+console.log("My favourite car is " + cars[2]);
+console.log("My favourite car is " + cars[0]);
+for (var i = 0; i <= 4; i++) {
+    console.log(cars[i]);
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+// for (let a = 1; a <= 100; ++a) {
+//   console.log("Thank you Governor Sindh Kamran Khan Tessori");
+// }
+
+const cars = ["Rolls Royce", "Bently","Ferrari","BMW"]
+console.log("My favourite car is " + cars[1]);
+console.log("My favourite car is " + cars[2]);
+console.log("My favourite car is " + cars[0]);
+for (let i = 0; i <= 4; i++){
+  console.log(cars[i]);
+}
